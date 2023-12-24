e123db86e1582e3f4c5518f2d403eff996be4111
Xersaix <xersaix@gmail.com>
2023-12-24 11:34:57 +0100

    added sound

--- Character/Hero/hero.gd
+++ Character/Hero/hero.gd
@@ -31,7 +31,7 @@ func _physics_process(delta):
 	move_and_slide()
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
-		if collision.get_collider().name == "tomb":
+		if collision.get_collider().name == "fire":
 			velocity.x = 0
 			state.travel("die")
 			
